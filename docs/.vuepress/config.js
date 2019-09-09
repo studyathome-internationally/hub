4c10cdc5f0c6599a2c74a28f64474228d7f20d20
Alija Sabic <sabic@technikum-wien.at>
2019-09-09 19:26:32 +0200

    Add markdown plugins and guide with demonstration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,89 +1,126 @@
-console.log("BASE", process.env.BASE);
+const { logger, chalk } = require("@vuepress/shared-utils");
+const twemoji = require("twemoji");
+
+const base = baseConfiguration();
+
 module.exports = {
   dest: "build",
-  base: process.env.BASE || "/",
+  base,
   title: "StudyATHome Internationally",
   theme: "@studyathome-internationally/hub",
+  extendMarkdown: md => markdownConfiguration(md),
   themeConfig: {
     // logo: "./assets/img/logos/studyathome-noir.svg",
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Project Teams", link: "/projectteams/" },
-      {
-        text: "More",
-        items: [
-          {
-            text: "eLearning Sharing",
-            items: [
-              { text: "Concepts", link: "/more/e-learning-sharing/concepts/" },
-              { text: "Guides", link: "/more/e-learning-sharing/guides/" },
-              { text: "Join!", link: "/more/e-learning-sharing/join" }
-            ]
-          },
-          {
-            text: "Virtual Project Teams",
-            items: [
-              { text: "Concepts", link: "/more/virtual-project-teams/concepts/" },
-              { text: "Guides", link: "/more/virtual-project-teams/guides/" },
-              { text: "Join!", link: "/more/virtual-project-teams/join" }
-            ]
-          },
-          {
-            text: "Hub",
-            items: [
-              // { text: "Concepts", link: "/more/hub/concepts/" },
-              { text: "Guides", link: "/more/hub/guides/" }
-              // { text: "Join!", link: "/more/hub/join" }
-            ]
-          },
-          {
-            text: "StudyATHome Internationally",
-            items: [
-              { text: "Contact", link: "/more/studyathome/contact" },
-              { text: "Partner", link: "/more/studyathome/partner" }
-            ]
-          },
-          {
-            text: "General",
-            items: [
-              { text: "Terms", link: "/more/general/terms" },
-              { text: "Privacy", link: "/more/general/privacy" },
-              { text: "Help", link: "/more/general/help" },
-              { text: "Sitemap", link: "/more/general/sitemap" },
-              { text: "About", link: "/more/general/about" }
-            ]
-          }
-        ]
-      }
-    ],
+    nav: navConfiguration(),
     displayAllHeaders: true,
-    sidebar: {
-      "/courses/": false,
-      "/more/e-learning-sharing/concepts/": [
-        ["concept1.md", "Concept 1"],
-        ["concept2.md", "Concept 2"],
-        ["concept3.md", "Concept 3"]
-      ],
-      "/more/e-learning-sharing/guides/": [
-        ["guide1.md", "Guide 1"],
-        ["guide2.md", "Guide 2"],
-        ["guide3.md", "Guide 3"]
-      ],
-      "/more/virtual-project-teams/concepts/": [
-        ["concept1.md", "Concept 1"],
-        ["concept2.md", "Concept 2"],
-        ["concept3.md", "Concept 3"]
-      ],
-      "/more/virtual-project-teams/guides/": [
-        ["guide1.md", "Guide 1"],
-        ["guide2.md", "Guide 2"],
-        ["guide3.md", "Guide 3"]
-      ],
-      "/more/hub/guides/": [
-        ["guide1.md", "Guide 1"],
-        ["guide2.md", "Guide 2"],
-        ["guide3.md", "Guide 3"]
-      ]
-    }
+    sidebar: sidebarConfiguration()
   }
 };
+
+function baseConfiguration() {
+  const base = process.env.BASE || "/";
+  logger.info(`Building site with base ${chalk.magenta(base)} ...`);
+  return base;
+}
+function markdownConfiguration(md) {
+  md.set({ breaks: true, typographer: true, linkify: true });
+  md.use(require("markdown-it-sub"));
+  md.use(require("markdown-it-sup"));
+  md.use(require("markdown-it-footnote"));
+  md.use(require("markdown-it-deflist"));
+  md.use(require("markdown-it-abbr"));
+  // md.use(require("markdown-it-emoji"));
+  md.use(require("markdown-it-checkbox"));
+  // md.use(require("markdown-it-imsize"), { autofill: true });
+  md.use(require("markdown-it-kbd"));
+
+  md.renderer.rules.emoji = function(token, idx) {
+    return twemoji.parse(token[idx].content, {
+      base: "https://twemoji.maxcdn.com/2/",
+      ext: ".svg",
+      folder: "svg"
+    });
+  };
+}
+function navConfiguration() {
+  return [
+    { text: "Home", link: "/" },
+    { text: "Project Teams", link: "/projectteams/" },
+    {
+      text: "More",
+      items: [
+        {
+          text: "eLearning Sharing",
+          items: [
+            { text: "Concepts", link: "/more/e-learning-sharing/concepts/" },
+            { text: "Guides", link: "/more/e-learning-sharing/guides/" },
+            { text: "Join!", link: "/more/e-learning-sharing/join" }
+          ]
+        },
+        {
+          text: "Virtual Project Teams",
+          items: [
+            { text: "Concepts", link: "/more/virtual-project-teams/concepts/" },
+            { text: "Guides", link: "/more/virtual-project-teams/guides/" },
+            { text: "Join!", link: "/more/virtual-project-teams/join" }
+          ]
+        },
+        {
+          text: "Hub",
+          items: [
+            // { text: "Concepts", link: "/more/hub/concepts/" },
+            { text: "Guides", link: "/more/hub/guides/" },
+            { text: "Markdown", link: "/more/hub/markdown" }
+          ]
+        },
+        {
+          text: "StudyATHome Internationally",
+          items: [
+            { text: "Contact", link: "/more/studyathome/contact" },
+            { text: "Partner", link: "/more/studyathome/partner" }
+          ]
+        },
+        {
+          text: "General",
+          items: [
+            { text: "Terms", link: "/more/general/terms" },
+            { text: "Privacy", link: "/more/general/privacy" },
+            { text: "Help", link: "/more/general/help" },
+            { text: "Sitemap", link: "/more/general/sitemap" },
+            { text: "About", link: "/more/general/about" }
+          ]
+        }
+      ]
+    }
+  ];
+}
+function sidebarConfiguration() {
+  return {
+    "/courses/": false,
+    "/more/e-learning-sharing/concepts/": [
+      ["concept1.md", "Concept 1"],
+      ["concept2.md", "Concept 2"],
+      ["concept3.md", "Concept 3"]
+    ],
+    "/more/e-learning-sharing/guides/": [
+      ["guide1.md", "Guide 1"],
+      ["guide2.md", "Guide 2"],
+      ["guide3.md", "Guide 3"]
+    ],
+    "/more/virtual-project-teams/concepts/": [
+      ["concept1.md", "Concept 1"],
+      ["concept2.md", "Concept 2"],
+      ["concept3.md", "Concept 3"]
+    ],
+    "/more/virtual-project-teams/guides/": [
+      ["guide1.md", "Guide 1"],
+      ["guide2.md", "Guide 2"],
+      ["guide3.md", "Guide 3"]
+    ],
+    "/more/hub/guides/": [
+      ["guide1.md", "Guide 1"],
+      ["guide2.md", "Guide 2"],
+      ["guide3.md", "Guide 3"]
+    ]
+  };
+}
---
+++ docs/more/hub/markdown.md
@@ -0,0 +1,284 @@
+---
+sidebar: auto
+sidebarDepth: 1
+---
+# HowTo Markdown
+
+## VuePress
+
+More details on the following extensions can be found [here](https://v1.vuepress.vuejs.org/guide/markdown.html#github-style-tables).
+
+---
+
+### Tables (GitHub)
+
+#### Input
+    | Tables        | Are           | Cool  |
+    | ------------- |:-------------:| -----:|
+    | col 3 is      | right-aligned | $1600 |
+    | col 2 is      | centered      |   $12 |
+    | zebra stripes | are neat      |    $1 |
+
+#### Result
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+---
+
+### Table of Contents
+
+#### Input
+    [[toc]]
+
+#### Result
+[[toc]]
+
+---
+
+### Custom Container
+
+#### Input
+    ::: tip
+    This is a tip
+    :::
+
+    ::: warning
+    This is a warning
+    :::
+
+    ::: danger STOP
+    This is a dangerous warning
+    :::
+
+#### Output
+::: tip
+This is a tip
+:::
+
+::: warning
+This is a warning
+:::
+
+::: danger STOP
+This is a dangerous warning
+:::
+
+---
+
+### Syntax Highlighting in Code Blocks
+
+#### Input
+    ``` js
+    export default {
+        name: 'MyComponent',
+        // ...
+        }```
+
+#### Output
+``` js
+export default {
+  name: 'MyComponent',
+  // ...
+}
+
+---
+```
+
+### Line Highlighting in Code Blocks
+
+#### Input
+    ``` js{4}
+    export default {
+    data () {
+        return {
+        msg: 'Highlighted!'
+        }
+    }
+    }
+    ```
+
+#### Output
+``` js{4}
+export default {
+  data () {
+    return {
+      msg: 'Highlighted!'
+    }
+  }
+}
+```
+
+## Extensions
+
+More details on the following extensions can be found here:
+
+* [markdown-it-sup](https://github.com/markdown-it/markdown-it-sup)
+* [markdown-it-sub](https://github.com/markdown-it/markdown-it-sub)
+* [markdown-it-footnote](https://github.com/markdown-it/markdown-it-footnote)
+* [markdown-it-deflist](https://github.com/markdown-it/markdown-it-deflist)
+* [markdown-it-abbr](https://github.com/markdown-it/markdown-it-abbr)
+* [markdown-it-emoji](https://github.com/markdown-it/markdown-it-emoji)
+  [twemoji](https://github.com/twitter/twemoji)
+* [markdown-it-checkbox](https://github.com/mcecot/markdown-it-checkbox)
+* [markdown-it-kbd](https://github.com/jGleitz/markdown-it-kbd)
+
+---
+
+### Superscript
+
+#### Input
+    29^th^
+
+#### Output
+29^th^
+
+---
+
+### Subscript
+
+#### Input
+    H~2~O
+
+#### Output
+H~2~O
+
+---
+
+### Footnote
+
+#### Input
+    Footnote 1 link[^first].
+
+    Footnote 2 link[^second].
+
+    Inline footnote^[Text of inline footnote] definition.
+
+    Duplicated footnote reference[^second].
+
+    [^first]: Footnote **can have markup**
+
+        and multiple paragraphs.
+
+    [^second]: Footnote text.
+
+#### Output
+Footnote 1 link[^first].
+
+Footnote 2 link[^second].
+
+Inline footnote^[Text of inline footnote] definition.
+
+Duplicated footnote reference[^second].
+
+[^first]: Footnote **can have markup**
+
+    and multiple paragraphs.
+
+[^second]: Footnote text.
+
+---
+
+### Definition Lists
+
+#### Input
+    Term 1
+
+    :   Definition 1
+    with lazy continuation.
+
+    Term 2 with *inline markup*
+
+    :   Definition 2
+
+            { some code, part of Definition 2 }
+
+        Third paragraph of definition 2.
+
+    _Compact style:_
+
+    Term 1
+    ~ Definition 1
+
+    Term 2
+    ~ Definition 2a
+    ~ Definition 2b
+
+#### Output
+Term 1
+
+:   Definition 1
+with lazy continuation.
+
+Term 2 with *inline markup*
+
+:   Definition 2
+
+        { some code, part of Definition 2 }
+
+    Third paragraph of definition 2.
+
+_Compact style:_
+
+Term 1
+  ~ Definition 1
+
+Term 2
+  ~ Definition 2a
+  ~ Definition 2b
+
+### Abbreviations
+
+#### Input
+    This is HTML abbreviation example.
+
+    It converts "HTML", but keep intact partial entries like "xxxHTMLyyy" and so on.
+
+    *[HTML]: Hyper Text Markup Language
+
+#### Output
+This is HTML abbreviation example.
+
+It converts "HTML", but keep intact partial entries like "xxxHTMLyyy" and so on.
+
+*[HTML]: Hyper Text Markup Language
+
+### Emoji :tada:
+
+::: tip Twemoji
+
+:copyright: 2019 Twitter, Inc and other contributors :bird: 
+Code licensed under the MIT License: http://opensource.org/licenses/MIT
+Graphics licensed under CC-BY 4.0: https://creativecommons.org/licenses/by/4.0/
+
+:::
+
+#### Input
+    *:tada:* :100: :nerd_face:
+
+#### Output
+**:tada:** :100: :nerd_face:
+
+A list of all emojis available can be found [here](https://github.com/markdown-it/markdown-it-emoji/blob/master/lib/data/full.json), [here](https://twemoji.maxcdn.com/2/test/preview.html) and [here](https://github.com/twitter/twemoji/) (`twemoji`).
+
+---
+
+### Checkboxes
+
+#### Input
+    [ ] unchecked
+    [x] checked
+
+#### Output
+[ ] unchecked
+[x] checked
+
+### Keystrokes
+
+#### Input
+    [[Strg]]+[[Alt]]+[[Entf]]
+
+#### Output
+[[Strg]]+[[Alt]]+[[Entf]]
