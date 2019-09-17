f0657c5347f9aaf6889b1c940ad13ac865284c8e
Alija Sabic <sabic@technikum-wien.at>
2019-09-17 16:35:17 +0200

    Add head configuration and pwa plugin, remove unused package

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,14 @@
 const { logger, chalk } = require("@vuepress/shared-utils");
-const twemoji = require("twemoji");
 
 const base = baseConfiguration();
 
 module.exports = {
   dest: "build",
   base,
+  head: headConfiguration(),
   title: "StudyATHome Internationally",
   theme: "@studyathome-internationally/hub",
+  plugins: ["@vuepress/pwa"],
   themeConfig: {
     logo: "./assets/img/logos/studyathome-noir.svg",
     nav: navConfiguration(),
@@ -20,6 +21,30 @@ function baseConfiguration() {
   const base = process.env.BASE || "/";
   logger.info(`Building site with base ${chalk.magenta(base)} ...`);
   return base;
+}
+function headConfiguration() {
+  return [
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "180x180", href: "assets/favicon/apple-touch-icon.png" }
+    ],
+    [
+      "link",
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "assets/favicon/favicon-32x32.png" }
+    ],
+    [
+      "link",
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "assets/favicon/favicon-16x16.png" }
+    ],
+    ["link", { rel: "manifest", href: "assets/favicon/site.webmanifest" }],
+    ["link", { rel: "mask-icon", href: "assets/favicon/safari-pinned-tab.svg", color: "#5bbad5" }],
+    ["link", { rel: "shortcut icon", href: "assets/favicon/favicon.ico" }],
+    ["meta", { name: "apple-mobile-web-app-title", content: "StudyATHome Internationally - Hub" }],
+    ["meta", { name: "application-name", content: "StudyATHome Internationally - Hub" }],
+    ["meta", { name: "msapplication-TileColor", content: "#00aba9" }],
+    ["meta", { name: "msapplication-config", content: "assets/favicon/browserconfig.xml" }],
+    ["meta", { name: "theme-c</meta>olor", content: "#ffffff" }]
+  ];
 }
 function navConfiguration() {
   return [
