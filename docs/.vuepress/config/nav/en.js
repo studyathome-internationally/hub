4e1d0474d6e05933c0ac1c14cc779316cf2c4611
Alija Sabic <sabic@technikum-wien.at>
2019-10-08 14:18:51 +0200

    Remove GitHub repository link, navigation related to Virtual Project Teams

--- docs/.vuepress/config/nav/en.js
+++ docs/.vuepress/config/nav/en.js
@@ -1,36 +1,48 @@
 module.exports = [
   { text: "Home", link: "/" },
   { text: "Courses", link: "/courses/" },
-  { text: "Project Teams", link: "/projectteams/" },
+  // { text: "Project Teams", link: "/projectteams/" },
   {
     text: "More",
     items: [
       {
         text: "eLearning Sharing",
         items: [
-          { text: "Concepts", link: "/e-learning-sharing/concepts/" },
-          { text: "Guides", link: "/e-learning-sharing/guides/" },
+          {
+            text: "Concepts",
+            link: "/e-learning-sharing/concepts/"
+          },
+          {
+            text: "Guides",
+            link: "/e-learning-sharing/guides/"
+          },
           { text: "FAQ", link: "/e-learning-sharing/faq" }
         ]
       },
-      {
-        text: "Virtual Project Teams",
-        items: [
-          { text: "Concepts", link: "/virtual-project-teams/concepts/" },
-          { text: "Guides", link: "/virtual-project-teams/guides/" },
-          { text: "FAQ", link: "/virtual-project-teams/faq" }
-        ]
-      },
+      // {
+      //   text: "Virtual Project Teams",
+      //   items: [
+      //     { text: "Concepts", link: "/virtual-project-teams/concepts/" },
+      //     { text: "Guides", link: "/virtual-project-teams/guides/" },
+      //     { text: "FAQ", link: "/virtual-project-teams/faq" }
+      //   ]
+      // },
       {
         text: "General",
         items: [
           { text: "Contact", link: "/studyathome/contact" },
-          { text: "Partner", link: "/studyathome/partner/" },
+          {
+            text: "Partner",
+            link: "/studyathome/partner/"
+          },
           { text: "Terms", link: "/studyathome/terms" },
           { text: "Privacy", link: "/studyathome/privacy" },
           { text: "Sitemap", link: "/studyathome/sitemap" },
           { text: "About", link: "/studyathome/about" },
-          { text: "Hub Editing", link: "/studyathome/markdown" }
+          {
+            text: "Hub Editing",
+            link: "/studyathome/markdown"
+          }
         ]
       }
     ]
