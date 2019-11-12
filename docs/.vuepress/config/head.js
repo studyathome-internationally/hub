77ff97dcf1a8e37d15276a0fdeb0b804760edc59
Alija Sabic <sabic.alija@gmail.com>
2019-11-12 17:46:32 +0100

    Add google search console id

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,22 +1,65 @@
 module.exports = [
   [
     "link",
-    { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicon/apple-touch-icon.png" }
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/assets/favicon/apple-touch-icon.png"
+    }
   ],
   [
     "link",
-    { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicon/favicon-32x32.png" }
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/assets/favicon/favicon-32x32.png"
+    }
   ],
   [
     "link",
-    { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicon/favicon-16x16.png" }
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/assets/favicon/favicon-16x16.png"
+    }
   ],
   ["link", { rel: "manifest", href: "/assets/manifest.json" }],
-  ["link", { rel: "mask-icon", href: "/assets/favicon/safari-pinned-tab.svg", color: "#5bbad5" }],
+  [
+    "link",
+    {
+      rel: "mask-icon",
+      href: "/assets/favicon/safari-pinned-tab.svg",
+      color: "#5bbad5"
+    }
+  ],
   ["link", { rel: "shortcut icon", href: "/assets/favicon/favicon.ico" }],
-  ["meta", { name: "apple-mobile-web-app-title", content: "StudyATHome Internationally - Hub" }],
-  ["meta", { name: "application-name", content: "StudyATHome Internationally - Hub" }],
+  [
+    "meta",
+    {
+      name: "apple-mobile-web-app-title",
+      content: "StudyATHome Internationally - Hub"
+    }
+  ],
+  [
+    "meta",
+    { name: "application-name", content: "StudyATHome Internationally - Hub" }
+  ],
   ["meta", { name: "msapplication-TileColor", content: "#00aba9" }],
-  ["meta", { name: "msapplication-config", content: "/assets/favicon/browserconfig.xml" }],
-  ["meta", { name: "theme-c</meta>olor", content: "#ffffff" }]
+  [
+    "meta",
+    {
+      name: "msapplication-config",
+      content: "/assets/favicon/browserconfig.xml"
+    }
+  ],
+  ["meta", { name: "theme-color", content: "#ffffff" }],
+  [
+    "meta",
+    {
+      name: "google-site-verification",
+      content: "qqt-J7hgZj0V-7cL1RH8tBhRdQajbXPlxBteXhfkTM0"
+    }
+  ]
 ];
