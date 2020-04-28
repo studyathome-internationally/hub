6dfd539fc5a21a23bc48db42537fc287d7355d2d
Martin Deinhofer <deinhofer.m@gmail.com>
2020-04-28 23:45:50 +0200

    added link for glossary

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -2,17 +2,21 @@ module.exports = {
   "/e-learning-sharing/": [
     {
       title: "Concept",
-      path: "/e-learning-sharing/"
+      path: "/e-learning-sharing/",
+    },
+    {
+      title: "Glossary",
+      path: "/e-learning-sharing/glossary.html",
     },
     {
       title: "FAQ",
-      path: "/e-learning-sharing/faq.html"
-    }
+      path: "/e-learning-sharing/faq.html",
+    },
   ],
   "/studyathome/partner/": [
     ["wit/", "WIT"],
     ["ctu/", "CTU"],
     ["ukim/", "UKIM"],
-    ["uastw/", "UAS TW"]
-  ]
+    ["uastw/", "UAS TW"],
+  ],
 };
