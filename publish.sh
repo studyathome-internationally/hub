9d174e066d07c3683300d62c2cd06e1ed5bf1a63
Martin Deinhofer <deinhofer.m@gmail.com>
2020-04-29 17:00:45 +0200

    publish to hub.studyathome.technikum-wien.at now, no basepath any more
    * echoing domain to CNAME file

--- publish.sh
+++ publish.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+BASEPATH="/"
+DOMAIN="hub.studyathome.technikum-wien.at"
+
 if [[ -z "$1" ]] ; then
     rm -rf build/ 
-    BASE="/hub-public/" yarn build 
+    BASE=$BASEPATH yarn build 
     mv gh-pages/next next-backup
     rm -rf gh-pages/* 
     cp -r build/* gh-pages/ 
     mv next-backup gh-pages/next 
-    cd gh-pages/ 
+    cd gh-pages/
+    echo $DOMAIN > CNAME
     git add . 
     git commit -m "docs: release hub" 
     git push
 else
     if [[ "$1" == "next" ]] ; then
         rm -rf build/ 
-        BASE="/hub-public/next/" yarn build 
+        BASE=$BASEPATH"next/" yarn build 
         rm -rf gh-pages/next
         cp -r build gh-pages/next
-        cd gh-pages/next 
+        cd gh-pages/next
+        echo $DOMAIN > CNAME
         git add . 
         git commit -m "docs: release hub" 
         git push
