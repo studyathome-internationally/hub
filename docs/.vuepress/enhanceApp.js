f0dd7f397f5970acbc3e2225dc16608f0bf17f80
Alija Sabic <sabic.alija@gmail.com>
2020-05-11 14:58:03 +0200

    Reload page components (custom theme) in development mode

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,16 @@
+export default async ({
+  Vue, // the version of Vue being used in the VuePress app
+  options, // the options for the root Vue instance
+  router, // the router instance for the app
+  siteData, // site metadata
+  isServer, // is this enhancement applied in server-rendering or client
+}) => {
+  if (process.env.NODE_ENV === "development" && !isServer) {
+    const { default: pageComponents } = await import("@internal/page-components");
+    // Hot reload page components,  in development mode, due to custom theme.
+    // > [Vue warn]: Unknown custom element: <v-xxxxxxxx> - did you register the component correctly?
+    for (const name in pageComponents) {
+      Vue.component(name, pageComponents[name]);
+    }
+  }
+};
