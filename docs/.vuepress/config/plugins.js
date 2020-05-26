3c91161c73a940e272b770f8eec7d899e8cd5884
Martin Deinhofer <deinhofer.m@gmail.com>
2020-05-26 11:31:03 +0200

    added try of excluding lecturer search but did not work

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -12,6 +12,7 @@ module.exports = [
       // searchMaxSuggestions: 1,
       test: [
         "^(?!/courses/(machine-vision|assistive-technologies-hands-on|computer-animation|embsys-vhdl-2|human-computer-interaction-design|multi-dimensional-data-analysis|template))",
+        // "^(?!/studyathome/partner/(ukim/lecturer/ivan-chorbev.html))",
       ],
     },
   ],
